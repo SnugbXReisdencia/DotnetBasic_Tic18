e09c9c878404812d282cb61a8a288d4405649b75
SnugbXReisdencia <santoseverlan45@gmail.com>
2023-11-19 21:10:14 -0300

    ADD pratica 02, criação e listagens de tarefas

---
+++ Semana2/IP-dotNET-P02/gerenciadorTarefas/model/GerenciarTarefas.cs
@@ -0,0 +1,182 @@
+﻿namespace gerenciadorTarefas;
+using tarefa;
+public class GerenciarTarefas
+{
+    List<Tarefa> tarefas = new List<Tarefa>();
+
+    public List<Tarefa> Tarefas { get => tarefas; }
+
+    public void CadastrarTarefa()
+    {
+        Console.Clear();
+        Console.WriteLine("####### Cadastrar Tarefa #######\n");
+
+        Tarefa tarefa = new Tarefa();
+        try
+        {
+            Console.Write("Título: ");
+            tarefa.Titulo = Console.ReadLine();
+            Console.Write("Descrição: ");
+            tarefa.Descricao = Console.ReadLine();
+            Console.Write("Data de Vencimento (dd/MM/yyyy): ");
+
+            tarefa.Data_Vencimento = DateTime.ParseExact(Console.ReadLine()!, "dd/MM/yyyy", null);
+            tarefas.Add(tarefa);
+        }
+        catch (Exception)
+        {
+            try
+            {
+                Console.WriteLine("Algum dado foi inserido de forma incorreta, tente novamente!");
+                Console.WriteLine("Caso queira cancelar, digite 0 para voltar ao menu principal: ");
+                if (int.Parse(Console.ReadLine()!) == 0)
+                    return;
+                CadastrarTarefa();
+            }
+            catch (Exception)
+            {
+                CadastrarTarefa();
+            }
+        }
+    }
+    public int qtd_TarefasPendentes()
+    {
+        int qtd = 0;
+        foreach (Tarefa tarefa in tarefas)
+        {
+            if (!tarefa.Status)
+            {
+                qtd++;
+            }
+        }
+        return qtd;
+    }
+
+    public int qtd_TarefasConcluidas()
+    {
+        int qtd = 0;
+        foreach (Tarefa tarefa in tarefas)
+        {
+            if (tarefa.Status)
+            {
+                qtd++;
+            }
+        }
+        return qtd;
+    }
+
+    public void ListarTarefasPendentes()
+    {
+        try
+        {
+            Console.Clear();
+            Console.WriteLine("###### Lista de Tarefas pendentes #####\n");
+            Console.WriteLine("###### Tarefas pendentes #####\n");
+            Console.WriteLine("Quantidade de tarefas pendentes: " + qtd_TarefasPendentes());
+            Console.WriteLine("\n");
+            foreach (Tarefa tarefa in tarefas)
+            {
+                if (!tarefa.Status)
+                {
+                    tarefa.Visualizar();
+                    Console.WriteLine("\n---------------------------------\n");
+                }
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Lista de tarefas pendentes vazia!");
+        }
+    }
+
+    public void ListarTarefasConcluidas()
+    {
+        try
+        {
+            Console.Clear();
+            Console.WriteLine("###### Lista de Tarefas concluídas #####\n");
+            Console.WriteLine("###### Tarefas concluídas #####\n");
+            Console.WriteLine("Quantidade de tarefas concluídas: " + qtd_TarefasConcluidas());
+            Console.WriteLine("\n");
+            foreach (Tarefa tarefa in tarefas)
+            {
+                if (tarefa.Status)
+                {
+                    tarefa.Visualizar();
+                    Console.WriteLine("\n---------------------------------\n");
+                }
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Lista de tarefas concluídas vazia!");
+        }
+    }
+
+    public void ListarTarefas()
+    {
+        try
+        {
+            Console.Clear();
+            Console.WriteLine("###### Estatísticas das Tarefas #####\n");
+            Console.WriteLine("Quantidade de tarefas: " + tarefas.Count);
+            Console.WriteLine("Quantidade de tarefas pendentes: " + qtd_TarefasPendentes());
+            Console.WriteLine("Quantidade de tarefas concluídas: " + qtd_TarefasConcluidas());
+            Console.WriteLine("Tarefas mais antigas: " + tarefas[0].Titulo);
+            Console.WriteLine("Tarefas mais recentes: " + tarefas[tarefas.Count - 1].Titulo);
+            Console.WriteLine("\nTodas as tarefas:\n");
+            foreach (Tarefa tarefa in tarefas)
+            {
+                tarefa.Visualizar();
+                Console.WriteLine("\n---------------------------------\n");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Lista de tarefas vazia!!");
+        }
+    }
+
+    public int BuscarTarefa()
+    {
+        try
+        {
+            Console.Clear();
+            Console.WriteLine("Qual tarefa deseja buscar?");
+            Console.Write("Título da tarefa: ");
+            string? str = Console.ReadLine();
+            if (!string.IsNullOrEmpty(str))
+            {
+                if (tarefas.Count > 0)
+                {
+                    foreach (Tarefa tarefa in tarefas)
+                    {
+                        if (tarefa.Titulo!.Equals(str))
+                        {
+                            Console.WriteLine("\n---------------------------------");
+                            tarefa.Visualizar();
+                            Console.WriteLine("\n---------------------------------");
+                            Console.WriteLine("É sua tarefa?(s/n)");
+                            if (Console.ReadLine()!.Equals("s"))
+                                return tarefas.IndexOf(tarefa);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception)
+        {
+            try
+            {
+                Console.WriteLine("Algo deu errado!");
+                Console.WriteLine("Deseja Tentar novamente? Caso queira cancelar, digite 0 para voltar ao menu principal: ");
+                if (int.Parse(Console.ReadLine()!) != 0)
+                {
+                    BuscarTarefa();
+                }
+            }catch (Exception){BuscarTarefa();}
+        }
+        return -1;
+    }
+
+}
---
+++ Semana2/IP-dotNET-P02/gerenciadorTarefas/model/Tarefa.cs
@@ -0,0 +1,50 @@
+﻿namespace tarefa;
+
+public class Tarefa
+{
+    string titulo;
+    string descricao;
+    DateTime data_Vencimento;
+    bool status;
+
+    public Tarefa(string titulo, string descricao, DateTime data_Vencimento)
+    {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data_Vencimento = data_Vencimento;
+        this.status = false;
+    }
+
+    public Tarefa()
+    {
+        this.titulo = "";
+        this.descricao = "";
+        this.status = false;
+    }
+    public string? Titulo { 
+        get {return this.titulo;}
+        set {this.titulo = value!;}
+    }
+    public string? Descricao {
+        get {return this.descricao;}
+        set {this.descricao = value!;}
+    }
+
+    public DateTime Data_Vencimento {
+        get {return this.data_Vencimento;}
+        set {this.data_Vencimento = value;}
+    }
+
+    public bool Status { 
+        get { return this.status; }
+        set { this.status = value; }
+    }
+
+    public void Visualizar()
+    {
+        Console.WriteLine("Titulo: " + this.titulo);
+        Console.WriteLine("Descrição: " + this.descricao);
+        Console.WriteLine("Data de Vencimento: " + this.data_Vencimento.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Status: " + (this.status ? "Concluido" : "Pendente"));
+    }
+}
