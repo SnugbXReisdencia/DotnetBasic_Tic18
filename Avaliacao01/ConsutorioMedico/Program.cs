d9b77fc0869e0f3bcb26ee7811c2e0a9d8f65a0f
SnugbXResidencia <santoseverlan45@gmail.com>
2023-11-29 12:36:49 -0300

    Commit obrigatorio 7

--- Avaliacao01/ConsutorioMedico/Program.cs
+++ Avaliacao01/ConsutorioMedico/Program.cs
@@ -5,13 +5,31 @@ using Modelos;
 
 List<Paciente> pacientes = new List<Paciente>();
 List<Medico> medicos = new List<Medico>();
-var appMedico = new AppMedico();
-var appPacientes = new AppPaciente();
+
 int opc;
-while(true){
-    switch(app.menu()){
+do{
+    opc = appMedico.menu();
+    switch(opc){
         case 1:
-            appMedico.criarMedico(medicos);
+            appMedico.App.criarMedico(medicos);
+            break;
+        case 2:
+            appMedico.App.criarPaciente(pacientes);
+            break;
+        case 3:
+            appMedico.App.imprimirMedicos(medicos);
+            break;
+        case 4:
+            appMedico.App.imprimirPacientes(pacientes);
+            break;
+        case 5:
+            appMedico.App.menuRelatorio();
+            break;
+        case 0:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Opcao invalida");
             break;
     }
-}
+}while(opc != 0);
