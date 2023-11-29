0f655eacfa39696b61fc4de20b86b225bb745176
SnugbXResidencia <santoseverlan45@gmail.com>
2023-11-29 10:45:48 -0300

    Commit obrigatorio Add estrutura basica

---
+++ Avaliacao01/ConsutorioMedico/src/modelos/Pessoa.cs
@@ -0,0 +1,19 @@
+namespace Modelos;
+public class Pessoa
+{
+    public String Nome { get; set; } = "";
+    public String Cpf { get { return this.Cpf;} set { if (ValidarCpf(Cpf)) { this.Cpf = Cpf;} } }
+
+    public DateTime DataNascimento { get; set; }
+
+    public bool ValidarCpf(string cpf)
+    {
+        if (string.IsNullOrEmpty(cpf)){
+            return false;
+        }
+        if (cpf.Length != 11){
+            return false;
+        }
+        return true;
+    }
+}
